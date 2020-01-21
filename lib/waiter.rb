e654a36fbd8e5de382356e4d6a73a51dd644f7c1
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-21 16:09:41 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,6 +16,7 @@ def meals
     |meal|
     meal.waiter == self 
   end 
+end 
   def best_tipper
     best_tipped_meal = 
     meals.max do |meal_a,meal_b|
@@ -26,5 +27,5 @@ best_tipped_meal.customer
 end 
   
 
-end
+
 end 
