48493e64185311bfba4227c26053fb4b69051226
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-21 15:47:36 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,5 +1,10 @@
 class Meal
-  def initialize(waiter,customer,total,tip)
+  def initialize(waiter,customer,total,tip=0)
+     @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
   end 
 
 end
